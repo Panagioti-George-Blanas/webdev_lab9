4ba218d9f145990510a53cd35dadd42e74d268bb
Panagioti-George-Blanas <156719339+Panagioti-George-Blanas@users.noreply.github.com>
2025-04-07 14:14:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
-  // Get DOM elements
-  const entryButton = document.getElementById('entrybutton');
-  const entryInput = document.getElementById('entryinput');
-  const textOutput = document.getElementById('textoutput');
+  var button = document.getElementById('entrybutton');
 
-  // Add event listener to the "Alert Me" button
-  entryButton.addEventListener('click', function () {
-    const inputText = entryInput.value;
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Panagioti Blanas: " + textbox.value);
+  }
 
-    // Show alert with your name and input
-    alert('Panagioti Blanas: ' + inputText); 
-
-    // Set the h2 text to just the input text
-    textOutput.textContent = inputText;
-  });
-}
-
+  button.addEventListener('click', showMeText);
+  }
+  
 window.addEventListener('load', init);
